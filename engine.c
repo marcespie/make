dbb118befbe1d53faac20686c39a2d11b5e39002
Marc Espie <espie@nerim.net>
2020-01-16 18:25:41 +0100

    fix a very nasty bug:
    set things up so that we know the initial signal mask on startup
    add a function to revert to that signal mask
    make sure we reset to that mask correctly between fork() and exec()

--- engine.c
+++ engine.c
@@ -769,6 +769,7 @@ do_run_command(Job *job, const char *pre)
 		Punt("Could not fork");
 		/*NOTREACHED*/
 	case 0:
+		reset_signal_mask();
 		/* put a random delay unless we're the only job running
 		 * and there's nothing left to do.
 		 */
